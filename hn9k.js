024e4406366987a26ebbb81b0aea02ef1cffe3c6
irjudson <irjudson@gmail.com>
2010-09-07 05:23:21 -0600

    Added options (the only option is the ip of the HN9k).

--- hn9k.js
+++ hn9k.js
@@ -1,5 +1,10 @@
 var req = new XMLHttpRequest();
-req.open("GET", "http://192.168.0.1/stlui/user/allowance_request.html");
+var address = localStorage["address"];
+if (!address) {
+  address = "192.168.0.1";
+  save_options();
+}
+req.open("GET", "http://"+address+"/stlui/user/allowance_request.html");
 req.onload = showStatus;
 req.send(null);
 
@@ -17,6 +22,7 @@ function showStatus() {
   data = data.getElementsByTagName("tr");
 
   // Make safe copies of the data to be used to make the panel
+  newData['HN-9000'] = address;
   newData['Max Download (MB)'] = TrimString(data[0].childNodes[2].textContent);
   newData['Remaining MB'] = TrimString(data[1].childNodes[2].textContent) + ' (' + TrimString(data[2].childNodes[2].textContent) + "%)";
   newData['Time Until Refilled'] = TrimString(data[3].childNodes[2].textContent);
@@ -41,4 +47,33 @@ function showStatus() {
 function TrimString(sInString) {
   sInString = sInString.replace( /^\s+/g, "" );// strip leading
   return sInString.replace( /\s+$/g, "" );// strip trailing
+}
+
+// Saves options to localStorage.
+function save_options() {
+  var address = document.getElementById("address").value;
+  localStorage["address"] = address;
+
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = "Options Saved.";
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 750);
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var address = localStorage["address"];
+  if (!address) {
+    address = "192.168.0.1";
+  }
+  var select = document.getElementById("address");
+  for (var i = 0; i < select.children.length; i++) {
+    var child = select.children[i];
+    if (child.value == address) {
+      child.selected = "true";
+      break;
+    }
+  }
 }
